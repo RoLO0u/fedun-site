3df7f7c6c9c0e55687f1d1215acdeea196dd92d1
RoLO0u <roman.fedunn@gmail.com>
2025-09-04 13:00:15 +0300

    small fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ import { Separator } from "@/components/ui/separator";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
+import { ExternalLinkIcon, MenuIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +24,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: { default: "Fedun.Site", template: "%s @ Fedun.Site" },
-  description: "Next.js + TypeScript + Tailwind CSS",
+  title: { default: "Fedun.Site", template: "Fedun.Site | %s" },
+  description: "Analyze chats, create sticker packs in telegram",
 };
 
 export default function RootLayout({
@@ -33,7 +35,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased p-5 flex flex-col min-h-screen 2xl:mx-32 xl:mx-20 md:mx-0`} suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
+        <div className="p-5 flex flex-col min-h-screen 2xl:mx-32 xl:mx-20">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -79,16 +82,55 @@ export default function RootLayout({
         {children}
         <footer className="mt-10 flex justify-center items-center">
           <div className="flex space-x-3 h-5 items-center">
-            <Link href="/" className="hover:underline">
+            <Link href="/" className="hover:underline hidden sm:block" >
               Fedun.Site
             </Link>
-            <Separator orientation="vertical" />
+            <Separator orientation="vertical" className="hidden sm:block" />
+            <Link href="/contact" className="hover:underline hidden sm:block">
+              Contact me
+            </Link>
+            <Separator orientation="vertical" className="hidden sm:block" />
             <Link href="/privacy" className="hover:underline">
               Privacy Policy
             </Link>
+            <Separator orientation="vertical" />
+            <Link href="https://github.com/RoLO0u/fedun-site" target="_blank" className="hover:underline sm:flex items-center gap-1 hidden">
+              Source Code <ExternalLinkIcon className="w-4 h-4 mb-0.5"/>
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="visible sm:hidden">
+                <MenuIcon className="h-6 w-6"/>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start" className="mr-5">
+                  <DropdownMenuGroup className="text-foreground text-xl flex flex-col gap-2 my-1.5">
+                    <Link href="/">
+                      <DropdownMenuItem className={`flex flex-row gap-1.5 hover:cursor-pointer hover:underline font-semibold mx-1 text-xl`}>
+                        <Image
+                          src="/logo.ico"
+                          alt="Home"
+                          width={32}
+                          height={32}
+                        />
+                        Fedun.Site 
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href="/contact" className="hover:underline">
+                      <DropdownMenuItem>
+                        Contact me
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href="https://github.com/RoLO0u/fedun-site" target="_blank" className="hover:underline flex items-center gap-1">
+                      <DropdownMenuItem>
+                        Source Code <ExternalLinkIcon className="w-4 h-4 mb-0.5"/>
+                      </DropdownMenuItem>
+                    </Link>
+                  </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </footer>
         </ThemeProvider>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
+import { ExternalLinkIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex flex-grow flex-col mt-5 gap-4">
       <h1 className="text-2xl font-bold">Welcome to Fedun.Site</h1>
       <div>
-        <Link href="/analyze" className="text-xl hover:underline">
+        <Link href="/analyze" className="text-xl flex hover:underline">
           Analyze telegram chats
         </Link>
         <p className="text-gray-500 dark:text-gray-400">Last updated: June 20, 2025</p>
       </div>
       <div>
         <Link href="https://t.me/paces_bot" target="_blank" className="text-xl hover:underline">
-          Sticker Packs Bot
+          <p className="flex gap-2 items-center">Sticker Packs Bot <ExternalLinkIcon className="w-5 h-5 mb-0.5"/></p>
         </Link>
         <p className="text-gray-500 dark:text-gray-400">Last updated: April 5, 2025</p>
       </div>
