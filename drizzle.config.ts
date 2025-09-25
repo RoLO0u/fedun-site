583ac6724ed2af299acabfcf8dad2abffda8317d
Roman Fedun <roman.fedunn@gmail.com>
2025-09-25 21:47:06 +0200

    add ssl

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from "drizzle-kit";
 
 config({ path: '.env' });
 
+const ssl = process.env.SSL_MODE === 'require' ? 'require' : false;
+
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl,
   },
 });
