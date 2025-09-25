e4071b67a2f985e148616bd1b326a22e3cbcca79
Roman Fedun <roman.fedunn@gmail.com>
2025-09-25 21:30:28 +0200

    Add signup button

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 const LoginPage = () => {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    const nameRef = useRef<HTMLInputElement>(null);
     const router = useRouter();
     return (
         <main className="flex flex-grow flex-col mt-5 gap-4 justify-center items-center" >
@@ -16,6 +17,8 @@ const LoginPage = () => {
             <div>Welcome to the admin page.</div>
             <Input id="email" className="md:w-80" type="email" placeholder="Email" ref={emailRef} />
             <Input id="password" className="md:w-80" type="password" placeholder="Password" ref={passwordRef} />
+            <Input id="name" className="md:w-80" type="text" placeholder="Login (Sign up only)" ref={nameRef} />
+            <div className="flex gap-2">
             <Button onClick={() => {
                 authClient.signIn.email(
                     {
@@ -31,6 +34,25 @@ const LoginPage = () => {
                     }
                 );
             }}>Sign In</Button>
+            <Button variant="secondary" onClick={() => {
+                authClient.signUp.email(
+                    {
+                        email: emailRef.current!.value,
+                        password: passwordRef.current!.value,
+                        name: nameRef.current!.value,
+                    }, {
+                        onSuccess: (ctx) => {
+                            alert("Account created! You can now log in.");
+                        },
+                        onError: (ctx) => {
+                            alert(ctx.error.message);
+                        },
+                    }
+                );
+            }} >
+                Sign Up
+            </Button>
+            </div>
         </main>
     )
 }
