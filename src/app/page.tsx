c139af9194e62d3934209bc5c7cf253fbe777351
Roman Fedun <roman.fedunn@gmail.com>
2025-09-25 22:14:57 +0200

    npm run build makes it static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { lastUpdatedType } from "@/lib/db/lastUpdatedTypes";
 import { getAllLastUpdated } from "@/lib/db/lastUpdatedActions";
 import HomePage from "@/components/homePage";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const lastUpdated = await getAllLastUpdated() as lastUpdatedType[];
   return <HomePage lastUpdatedData={lastUpdated} />;
